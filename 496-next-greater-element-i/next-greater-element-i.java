eff795a6ed70d643c15c105cc556dd2018c87ca1
vtu25957-cpu <vtu25957@veltech.edu.in>
2026-02-11 10:22:56 +0530

    Time: 3 ms (92.81%) | Memory: 45.4 MB (46.09%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Process nums2
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreater.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        // Remaining elements have no next greater
+        while (!stack.isEmpty()) {
+            nextGreater.put(stack.pop(), -1);
+        }
+
+        // Build result for nums1
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreater.get(nums1[i]);
+        }
+
+        return result;
+    }
+}
