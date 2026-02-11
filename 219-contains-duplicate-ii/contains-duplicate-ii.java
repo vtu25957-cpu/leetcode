30a59045832bd93a50cf39b85cbd38b20c385635
vtu25957-cpu <vtu25957@veltech.edu.in>
2026-02-11 11:13:56 +0530

    Time: 23 ms (81.27%) | Memory: 91 MB (9.58%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int prevIndex = map.get(nums[i]);
+                
+                if (i - prevIndex <= k) {
+                    return true;
+                }
+            }
+            
+            // Update latest index
+            map.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}
