f7708735a1983c6b7aec4dd2fe705a95638fa541
vtu25957-cpu <vtu25957@veltech.edu.in>
2026-02-11 10:21:44 +0530

    Time: 21 ms (79.59%) | Memory: 59.7 MB (33.98%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class RecentCounter {
+    private Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        q.offer(t);  // Add the new ping
+        // Remove pings older than t - 3000
+        while (q.peek() < t - 3000) {
+            q.poll();
+        }
+        return q.size();
+    }
+}
