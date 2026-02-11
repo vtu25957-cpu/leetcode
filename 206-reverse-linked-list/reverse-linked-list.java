b869bc961a40567f66310ca9ee85d91e957c9910
vtu25957-cpu <vtu25957@veltech.edu.in>
2026-02-11 10:56:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (65.70%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next; 
+            curr.next = prev;              
+            prev = curr;                   
+            curr = nextNode;               
+        }
+
+        return prev; 
+    }
+}
